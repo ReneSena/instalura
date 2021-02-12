0a2841126892b79446cbbb122fe55d8c01dd50f9
ReneSena <rene.sena3005@gmail.com>
2021-02-11 23:22:50 -0300

    Adição de reset css com normalize e importação da fonte Rubik do google fontes.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import Menu from "../src/components/commons/Menu";
+import Footer from "../src/components/commons/Footer";
+
+const Container = styled.div`
+	flex: 1;
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	justify-content: space-between;
+`;
 
 export default function Home() {
 	return (
-		<>
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link
-				href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-				rel="stylesheet"
-			/>
+		<Container>
 			<Menu />
-		</>
+			<Footer />
+		</Container>
 	);
 }
