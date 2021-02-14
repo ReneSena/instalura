70e51544a3bdb5dfba9175e8085ddb4a6ae0a063
ReneSena <rene.sena3005@gmail.com>
2021-02-14 14:48:22 -0300

    Criação de componente de Box.

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,39 @@
 import React from "react";
-import styled from "styled-components";
 import Menu from "../src/components/commons/Menu";
 import Footer from "../src/components/commons/Footer";
 import Text from "../src/components/foundation/Text";
 import { Button } from "../src/components/commons/Button";
 import { Grid } from "../src/components/foundation/layout/Grid";
-
-const Container = styled.div`
-	flex: 1;
-	display: flex;
-	flex-wrap: wrap;
-	flex-direction: column;
-	justify-content: space-between;
-`;
+import { Box } from "../src/components/foundation/layout/Box";
 
 export default function Home() {
 	return (
-		<Container>
+		<Box
+			flex="1"
+			display="flex"
+			flexWrap="wrap"
+			flexDirection="column"
+			justifyContent="space-between"
+			backgroundImage="url(/images/bubbles.svg)"
+			backgroundRepeat="no-repeat"
+			backgroundPosition="bottom right"
+		>
 			<Menu />
 
-			<Grid.Container>
+			<Grid.Container
+				marginTop={{
+					xs: "32px",
+					md: "75px",
+				}}
+			>
 				<Grid.Row>
 					<Grid.Col
 						value={{ xs: 12, md: 5 }}
 						offset={{ xs: 0, md: 1 }}
+						display="flex"
+						alignItems="flex-start"
+						justifyContent="center"
+						flexDirection="column"
 					>
 						<Text
 							variant="title"
@@ -76,6 +86,6 @@ export default function Home() {
 				</Grid.Row>
 			</Grid.Container>
 			<Footer />
-		</Container>
+		</Box>
 	);
 }
---
+++ src/components/foundation/layout/Box/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { propToStyle } from "../../../../theme/propToStyle";
+
+export const Box = styled.div`
+	${propToStyle("flex")}
+	${propToStyle("display")}
+	${propToStyle("flexDirection")}
+	${propToStyle("justifyContent")}
+	${propToStyle("flex")}
+	${propToStyle("flexWrap")}
+	${propToStyle("backgroundImage")}
+	${propToStyle("backgroundRepeat")}
+	${propToStyle("backgroundPosition")}
+`;
