0008df73169ee89120f960e25db78511e40b855c
ReneSena <rene.sena3005@gmail.com>
2021-07-21 00:49:54 -0300

    feat(data cms): created the about page and Integration with dato cms

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-function PageAbout() {
-	return <div>Página Sobre</div>;
+export async function getStaticProps() {
+	const TOKEN = process.env.DATA_CMS_TOKEN;
+	const DatoCMSURL = 'https://graphql.datocms.com/';
+
+	const client = new GraphQLClient(DatoCMSURL, {
+		headers: {
+			Authorization: `Bearer ${TOKEN}`,
+		},
+	});
+
+	const query = gql`
+		query {
+			pageabout {
+				pageTitle
+				pageDescription
+			}
+		}
+	`;
+
+	const messages = await client.request(query);
+
+	return {
+		props: {
+			messages,
+		},
+	};
 }
 
-export default PageAbout;
+export default websitePageHOC(AboutScreen);
